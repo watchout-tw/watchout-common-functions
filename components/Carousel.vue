092dce5c8720353ab650adcda56314c87f995e8c
chihaoyo <chihaoyo@gmail.com>
2018-03-23 18:31:10 +0800

    Update Carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,20 +1,75 @@
 <template>
 <div class="carousel">
   <div class="content">
-    <div class="page">
-      什麼是給問擂台？
+    <div class="pages" :id="scrollElementID">
+      <div class="page" v-for="page of pages" :style="pageStyles(page)"></div>
     </div>
+  </div>
+  <div class="controls">
+    <div class="control prev" @click="goToPage((currentPage + pages.length - 1) % pages.length)"></div>
+    <div class="control next" @click="goToPage((currentPage + pages.length + 1) % pages.length)"></div>
   </div>
 </div>
 </template>
 
 <script>
+const genID = require('uuid/v4')
+/* FIXME: Current assumptions
+
+- A carousel sets its own size ONLY at DOM ready
+- A carousel is 2-to-1 on smaller devices & 4-to-1 on larger decives with a 576px breakpoint
+
+*/
 export default {
-  props: ['config'],
-  computed: {
-    aspectRatio() {
-      return this.config && this.config.hasOwnProperty('aspectRatio') ? this.config.aspectRatio : 16.0 / 9.0
+  props: ['pages'],
+  data() {
+    return {
+      bp: {
+        sm: 576
+      },
+      currentPage: 0,
+      scrollElementID: genID(),
+      windowSize: {
+        width: 0,
+        height: 0
+      }
     }
+  },
+  computed: {
+    scrollElement() {
+      return document ? document.getElementById(this.scrollElementID) : null
+    },
+    scrollLeft() {
+      return this.currentPage * this.scrollElement.offsetWidth // getBoundingClientRect().width
+    }
+  },
+  watch: {
+    currentPage() {
+      if(this.scrollElement) {
+        this.scrollElement.scrollLeft = this.scrollLeft
+      }
+    }
+  },
+  methods: {
+    updateWindowSize() {
+      if(window) {
+        this.windowSize.height = window.innerHeight
+        this.windowSize.width = window.innerWidth
+      }
+    },
+    pageStyles(page) {
+      var styles = {}
+      if(page.type === 'image') {
+        styles.backgroundImage = `url(${this.windowSize.width > this.bp.sm ? page.urls.wide : page.urls.default})`
+      }
+      return styles
+    },
+    goToPage(index) {
+      this.currentPage = index
+    }
+  },
+  mounted() {
+    this.updateWindowSize()
   }
 }
 </script>
@@ -29,8 +84,55 @@ export default {
     @include rect(4/1)
   }
   > .content {
-    > .page {
-      width: 100%;
+    > .pages {
+      height: 100%;
+      display: flex;
+      overflow-x: auto;
+      align-items: stretch;
+      -webkit-overflow-scrolling: touch;
+      > .page {
+        flex-shrink: 0;
+        width: 100%;
+        background-size: cover;
+        background-position: center center;
+      }
+    }
+  }
+  > .controls {
+    position: absolute;
+    display: flex;
+    align-items: stretch;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    > .control {
+      flex-grow: 1;
+      cursor: pointer;
+      &.prev {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        &:before {
+          content: '◀︎';
+          display: block;
+          margin: 0 0.5rem;
+          font-size: 1.5rem;
+          opacity: 0.25;
+        }
+      }
+      &.next {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        &:after {
+          content: '▶︎';
+          display: block;
+          margin: 0 0.5rem;
+          font-size: 1.5rem;
+          opacity: 0.25;
+        }
+      }
     }
   }
 }
