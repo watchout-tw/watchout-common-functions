e8adf4f3d99a65efa92f0f2edb999374f7cdbc17
chihaoyo <chihaoyo@gmail.com>
2018-04-13 17:40:47 +0800

    Update TextEditor; add simple mode

--- components/TextEditor.vue
+++ components/TextEditor.vue
@@ -1,19 +1,38 @@
 <template>
-<div class="text-editor">
+<div class="text-editor" :class="computedClasses">
   <div class="tools"></div>
   <div class="content">
-    <textarea rows="4" ref="contentInput" :placeholder="placeholder" :value="content" @input="updateContent()" :maxlength="maxlength"></textarea>
+    <component :is="inputElement" rows="4" ref="inputElement" :placeholder="placeholder" :value="content" @input="updateContent()" @focus="isFocused = true" @blur="isFocused = false" :maxlength="maxlength"></component>
   </div>
 </div>
 </template>
 
 <script>
 export default {
-  props: ['placeholder', 'content', 'maxlength'],
+  props: ['simple', 'placeholder', 'content', 'maxlength', 'classes'],
+  data() {
+    return {
+      isFocused: false
+    }
+  },
+  computed: {
+    inputElement() {
+      return this.simple ? 'input' : 'textarea'
+    },
+    computedClasses() {
+      let moreClasses = []
+      if(this.simple) {
+        moreClasses.push('simple')
+      }
+      if(this.isFocused) {
+        moreClasses.push('focused')
+      }
+      return Array.isArray(this.classes) ? this.classes.concat(...moreClasses) : moreClasses
+    }
+  },
   methods: {
-    updateContent () {
-      const text = this.$refs.contentInput.value
-      this.$emit('input', text)
+    updateContent() {
+      this.$emit('input', this.$refs.inputElement.value)
     }
   }
 }
@@ -23,15 +42,41 @@ export default {
 @import '~watchout-common-assets/styles/resources';
 
 .text-editor {
-  border: 2px $color-border-grey solid;
+  border-style: solid;
+  border-color: $color-light-border-grey;
   border-radius: 1px;
+  &.simple {
+    border-width: 0;
+    border-bottom-width: 1px;
+  }
+  &:not(.simple) {
+    border-width: 1px;
+  }
+  &.focused {
+    background-color: $color-watchout-very-light;
+    border-color: $color-watchout;
+    &.ask {
+      border-color: $color-ask;
+    }
+    &.park {
+      border-color: $color-park;
+    }
+  }
   > .content {
+    > input {
+      display: block;
+      width: 100%;
+      border: none;
+      background: none;
+      padding: 0.25rem 0.375rem;
+    }
     > textarea {
       display: block;
       width: 100%;
       border: none;
       padding: 0.25rem 0.375rem;
       line-height: $line-height-default;
+      background: none;
     }
   }
 }
