80199c0adfd625e99c2a05a55ef630f6ac99f2c8
chihaoyo <chihaoyo@gmail.com>
2018-04-16 12:18:19 +0800

    Add CoverImage

---
+++ components/CoverImage.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="cover-image" :style="styles">
+</div>
+</template>
+
+<script>
+export default {
+  props: ['url'],
+  computed: {
+    image() {
+      if(this.url.includes('http')) {
+        return this.url
+      } else {
+        return 'https://raw.githubusercontent.com/watchout-tw/watchout-common-assets/master/images/' + this.url
+      }
+    },
+    styles() {
+      return {
+        backgroundImage: `url(${this.image})`
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+.cover-image {
+  @include rect(4/1);
+  background-size: cover;
+  background-position: center center;
+}
+</style>
