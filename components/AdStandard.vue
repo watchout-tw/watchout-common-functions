1ca244b7cbdfe928b50b9151f8d521cabe395761
chihaoyo <chihaoyo@gmail.com>
2018-04-19 18:59:11 +0800

    Hide content box when empty

--- components/AdStandard.vue
+++ components/AdStandard.vue
@@ -1,18 +1,24 @@
 <template>
 <div class="ad standard">
-  <div class="content">
+  <div class="content" v-if="data">
     <div class="heading section-title-underline-tiny text-align-center"><span>廣告</span></div>
   </div>
 </div>
 </template>
 
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
 <style lang="scss">
 @import '~watchout-common-assets/styles/resources';
 .ad.standard {
-  @include rect(4/1);
-  border: 2px $color-very-light-grey solid;
-  border-radius: 1px;
   > .content {
+    @include rect(4/1);
+    border: 2px $color-very-light-grey solid;
+    border-radius: 1px;
     > .heading {
       margin: 0.25rem 0;
     }
