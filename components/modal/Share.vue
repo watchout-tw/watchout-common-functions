121ad8ed66445369e308092c4b7fc41215a3b810
Wesley <wesley100002001@gmail.com>
2018-04-23 15:48:29 +0800

    Implement Share modal
    1. add facebook, twitter share link
    2. styling (not finished yet)

--- components/modal/Share.vue
+++ components/modal/Share.vue
@@ -1,19 +1,38 @@
 <template>
 <div class="modal share">
-  <div :style="facebook"></div>
-  {{ data.url }}
+<h2>分享到</h2>
+  <a target="_blank" :href="twitterShare"><div class="button social" :style="twitter"></div></a>
+  <a target="_blank" :href="facebookShare"><div class="button social" :style="facebook"></div></a>
+  <a><div class="button social" :style="line"></div></a>
 </div>
 </template>
 
 <script>
+import facebook from 'watchout-common-assets/images/platforms/facebook.png'
+import line from 'watchout-common-assets/images/platforms/line.png'
+import twitter from 'watchout-common-assets/images/platforms/twitter.png'
 
 export default {
   props: ['data'],
-  computed: {
-    facebook () {
-      return {
-        backgroundImage: 'url(' + require('watchout-common-assets/images/platforms/facebook.png') + ')'
+  data () {
+    return {
+      facebook: {
+        backgroundImage: `url(${facebook})`
+      },
+      line: {
+        backgroundImage: `url(${line})`
+      },
+      twitter: {
+        backgroundImage: `url(${twitter})`
       }
+    }
+  },
+  computed: {
+    facebookShare () {
+      return `https://www.facebook.com/sharer/sharer.php?u=${this.data.url}`
+    },
+    twitterShare () {
+      return `https://twitter.com/intent/tweet?url=${this.data.url}`
     }
   }
 }
@@ -28,5 +47,10 @@ export default {
   padding: 1rem;
   background-color: $color-park-light;
   @include shadow;
+}
+.social {
+  background-size: contain;
+  width: 4rem;
+  height: 4rem;
 }
 </style>
