66fdfaa00344547b46eef381a31328037d81d8a4
chihaoyo <chihaoyo@gmail.com>
2018-04-25 12:50:58 +0800

    Add Welcome

---
+++ components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="welcome">{{ activePersona && activePersona.name ? '歡迎回來，' + activePersona.name : '歡迎來到沃草共有地' }}。</div>
+</template>
+
+<script>
+import { knowsAuth } from 'watchout-common-functions/interfaces'
+export default {
+  mixins: [knowsAuth]
+}
+</script>
+
+<style lang="scss">
+.welcome {
+  text-align: center;
+}
+</style>
