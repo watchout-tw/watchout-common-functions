f93ed84cf861a9d981ffd54050ab2d60b6e84ada
mia81514 <mia81514@gmail.com>
2018-05-21 14:43:39 +0800

    can randomly get 4 options from all options

--- components/CoverImageBrowser.vue
+++ components/CoverImageBrowser.vue
@@ -8,19 +8,35 @@
 
 <script>
 const defaultOptions = [
-  'watchout-common-assets/images/cover-images/1.png',
-  'watchout-common-assets/images/cover-images/2.png',
-  'watchout-common-assets/images/cover-images/3.png',
-  'watchout-common-assets/images/cover-images/4.png',
-  'watchout-common-assets/images/cover-images/5.png',
-  'watchout-common-assets/images/cover-images/6.png'
+  'watchout-common-assets/images/cover-images/1.jpeg',
+  'watchout-common-assets/images/cover-images/2.jpeg',
+  'watchout-common-assets/images/cover-images/3.jpeg',
+  'watchout-common-assets/images/cover-images/4.jpeg',
+  'watchout-common-assets/images/cover-images/5.jpg',
+  'watchout-common-assets/images/cover-images/6.jpeg'
 ]
+const optionsAmt = 4
 
 export default {
   props: ['limit', 'selectedOptions', 'options'],
-  computed: {
-    internalOptions() {
-      return Array.isArray(this.options) ? this.options : defaultOptions
+  data() {
+    return {
+      internalOptions: []
+    }
+  },
+  mounted() {
+    this.selectOptions()
+  },
+  methods: {
+    selectOptions() {
+      const options = Array.isArray(this.options) ? this.options : defaultOptions
+      var indexes = []
+      while(indexes.length < optionsAmt) {
+        var index = Math.floor(Math.random() * options.length)
+        if (indexes.indexOf(index) > -1) continue
+        indexes.push(index)
+      }
+      this.internalOptions = indexes.map(index => options[index])
     }
   }
 }
