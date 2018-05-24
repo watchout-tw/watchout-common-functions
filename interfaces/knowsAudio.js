b2ebdb27a6f711e607e896bbf01a9511a0fd058d
mia81514 <mia81514@gmail.com>
2018-05-24 12:21:52 +0800

    interface - knowsAudio

---
+++ interfaces/knowsAudio.js
@@ -0,0 +1,22 @@
+export default {
+  methods: {
+    playAudio(el) {
+      el.currentTime = 0.0
+      el.volume = 1.0
+      el.play()
+    },
+    fadeOutAudio(el, callback) {
+      var volume = el.volume
+      var timer = setInterval(function() {
+        volume = (parseFloat(volume) - 0.1).toFixed(1)
+        if(volume >= 0) {
+          el.volume = volume
+        } else {
+          el.pause()
+          clearInterval(timer)
+          callback()
+        }
+      }, 50)
+    }
+  }
+}
