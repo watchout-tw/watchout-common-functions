b0d5b7b53a407bcf6e46714edeb6d1190e0cf17d
chihaoyo <chihaoyo@gmail.com>
2018-06-02 11:08:57 +0800

    Add hints to GenderSlider

--- components/GenderSlider.vue
+++ components/GenderSlider.vue
@@ -1,18 +1,39 @@
 <template>
 <div class="gender-slider input">
-  <input type="range" min="-100" max="100" step="1" v-model="model" @change="push" />
-  <div class="track">
-    <div class="nob" :style="{ left: `${position}%` }"></div>
+  <div class="track-container">
+    <div class="track"></div>
+    <div class="hints secondary-text font-size-tiny">
+      <div class="hint" v-for="hint of hints" :style="getPosition(hint.value)">
+        <div class="dot"></div>
+        <div class="label">{{ hint.label }}</div>
+      </div>
+      <div class="hint queer"><div class="dot"></div></div>
+    </div>
   </div>
-  <div class="label">{{ label }}</div>
+  <input type="range" min="-100" max="100" step="1" v-model="model" @change="push" />
+  <div class="nob-container">
+    <div class="nob" :style="getPositionFromLeft(model)"></div>
+  </div>
+  <div class="label"><span v-if="percentage !== 0" class="latin-within-han first">{{ Math.abs(percentage) }}%</span><span>{{ label }}</span></div>
 </div>
 </template>
 
 <script>
+let hints = [
+  {
+    value: 100,
+    label: '男性'
+  },
+  {
+    value: -100,
+    label: '女性'
+  }
+]
 export default {
   props: ['value'],
   data() {
     return {
+      hints,
       model: this.value ? this.value : 0
     }
   },
@@ -22,17 +43,31 @@ export default {
     }
   },
   computed: {
-    position() {
-      return (parseInt(this.model) + 100) / 2
-    },
     percentage() {
-      return Math.abs(parseInt(this.model)) + '%'
+      return parseInt(this.model)
     },
     label() {
-      return parseInt(this.model) === 0 ? '酷兒' : this.percentage + (this.model > 0 ? '男性' : '女性')
+      return this.percentage === 0 ? '酷兒' : (this.percentage > 0 ? '男性' : '女性')
     }
   },
   methods: {
+    getPositionFromLeft(val) {
+      return {
+        left: ((parseInt(val) + 100) / 2) + '%'
+      }
+    },
+    getPosition(val) {
+      let from = 'left'
+      let pos = ((parseInt(val) + 100) / 2)
+      if(pos > 50) {
+        from = 'right'
+        pos = 100 - pos
+      }
+      return {
+        [from]: pos + '%',
+        transform: 'translateX(' + (from === 'left' ? '-' : '+') + '50%)'
+      }
+    },
     pull() {
       this.model = this.value ? this.value : 0
     },
@@ -48,26 +83,83 @@ export default {
 
 .gender-slider {
   $track-height: 0.125rem;
+  $track-color: $color-border-grey;
   $nob-size: 1.5rem;
   $input-height: $nob-size * 1;
+  $hint-dot-size: 0.75rem;
+
   text-align: center;
   position: relative;
   margin: 0 $nob-size / 2;
-
+  > .track-container {
+    height: $input-height;
+    > .track {
+      position: absolute;
+      top: ($input-height - $track-height) / 2;
+      left: 0;
+      width: 100%;
+      height: $track-height;
+      background-color: $track-color;
+    }
+    > .hints {
+      position: absolute;
+      width: 100%;
+      > .hint {
+        position: absolute;
+        > .dot {
+          width: $hint-dot-size;
+          height: $hint-dot-size;
+          border-radius: 50%;
+          margin: #{($input-height - $hint-dot-size) / 2} auto;
+          background-color: $track-color;
+        }
+      }
+      > .hint.queer {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        > .dot {
+          $color-rainbow-red: #EF606A;
+          $color-rainbow-orange: #F4B456;
+          $color-rainbow-yellow: #F9E341;
+          $color-rainbow-green: #41EF5E;
+          $color-rainbow-blue: #6BBCF9;
+          $color-rainbow-purple: #B66CEF;
+          background: linear-gradient(
+            180deg,
+            $color-rainbow-red 0%,
+            $color-rainbow-red 16.6667%,
+            $color-rainbow-orange 16.6667%,
+            $color-rainbow-orange 33.3333%,
+            $color-rainbow-yellow 33.3333%,
+            $color-rainbow-yellow 50%,
+            $color-rainbow-green 50%,
+            $color-rainbow-green 66.6667%,
+            $color-rainbow-blue 66.6667%,
+            $color-rainbow-blue 83.3333%,
+            $color-rainbow-purple 83.3333%,
+            $color-rainbow-purple 100%
+          );
+        }
+      }
+    }
+  }
   > input {
-    position: relative;
+    position: absolute;
     opacity: 0;
+    top: 0;
+    left: 0;
     width: 100%;
     height: $input-height;
     z-index: 2;
+    cursor: pointer;
   }
-  > .track {
+  > .nob-container {
     position: absolute;
-    top: $input-height / 2;
+    top: ($input-height - $track-height) / 2;
     left: 0;
     width: 100%;
-    height: $track-height;
-    background-color: $color-border-grey;
     z-index: 1;
     > .nob {
       position: absolute;
