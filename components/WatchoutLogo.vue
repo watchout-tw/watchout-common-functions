7ffa8277e7f93d81ed2f33728a2de985acdd46a8
chihaoyo <chihaoyo@gmail.com>
2018-07-10 11:33:12 +0800

    Add WatchoutLogo

---
+++ components/WatchoutLogo.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="watchout-logo">
+  <img class="image" :src="image" :width="width" />
+</div>
+</template>
+
+<script>
+import logoBlack from 'watchout-common-assets/images/watchout-logo/black.png'
+export default {
+  data() {
+    return {
+      image: logoBlack,
+      width: 194
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+
+.watchout-logo {
+  padding: 1rem;
+  > .image {
+    margin: 0 auto;
+  }
+}
+</style>
