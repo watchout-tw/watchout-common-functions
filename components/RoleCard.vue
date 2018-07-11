02d734437f701ab5b04a63802cbee3ebfd3b5530
chihaoyo <chihaoyo@gmail.com>
2018-07-11 23:35:10 +0800

    Add RoleCard; add lib/roles

---
+++ components/RoleCard.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="role-card">
+  <div class="secondary-text font-size-small">目前角色</div>
+  <div>{{ roleText }}</div>
+</div>
+</template>
+
+<script>
+import { knowsAuth } from 'watchout-common-functions/interfaces'
+
+export default {
+  mixins: [knowsAuth],
+  computed: {
+    roleText() {
+      let text = '路人'
+      if(this.isLocal() && Array.isArray(this.roles)) {
+        if(this.hasRole(null, 'with_info')) {
+          text = '大草民'
+        } else {
+          text = '草民'
+        }
+      }
+      return text
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+
+.role-card {
+  padding: 0.75rem;
+  background-color: $color-park-light;
+}
+</style>
