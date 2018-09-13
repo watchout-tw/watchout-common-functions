33128d6fdf736c70e205bf669a5ee8872b2c90a4
chihaoyo <chihaoyo@gmail.com>
2018-09-14 00:47:15 +0800

    Add ghost to config

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,6 +1,8 @@
 export default {
   coreBaseURL: 'https://sample.watchout.tw',
   coralreefBaseURL: 'https://sample.watchout.tw',
-  reCaptchaSiteKey: 'google-re-captcha-site-key'
+  reCaptchaSiteKey: 'google-re-captcha-site-key',
+  ghostClient: 'ghost-client-slug',
+  ghostClientSecret: 'ghost-client-secret',
   env: 'dev' // dev OR beta OR production
 }
