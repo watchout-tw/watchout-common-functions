bbdcc396bb321894c7d45a451a241d9fbd422da0
chihaoyo <chihaoyo@gmail.com>
2018-11-08 13:06:31 +0900

    Update config/config

--- config/config.sample.js
+++ config/config.sample.js
@@ -2,6 +2,7 @@ export default {
   coreBaseURL: 'https://sample.watchout.tw',
   coralreefBaseURL: 'https://sample.watchout.tw',
   reCaptchaSiteKey: 'google-re-captcha-site-key',
+  reCaptchaType: 'invisible',
   ghostClient: 'ghost-client-slug',
   ghostClientSecret: 'ghost-client-secret',
   env: 'dev' // dev OR beta OR production
