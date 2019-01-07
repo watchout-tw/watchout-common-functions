e85680765df27d6bc34d281115ab2d4f25165dbe
chihaoyo <chihaoyo@gmail.com>
2019-01-07 18:19:55 +0800

    Disable GhostMarkup

--- components/ghost/Article.vue
+++ components/ghost/Article.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="ghost-article center-self with-left-right-margin">
   <h1 class="title small">{{ article.title }}</h1>
-  <div class="doc">
-    <template v-for="(section, index) of doc.sections">
-      <ghost-markup v-if="section[0] === 1" :key="index" :tag-name="section[1]" :markers="section[2]" :atoms="doc.atoms" :markups="doc.markups" />
-      <ghost-card v-else-if="section[0] === 10" :key="index" :card="doc.cards[section[1]]" :data="data" />
+  <div class="content">
+    <template v-for="(section, index) of mobiledoc.sections">
+      <!--<ghost-markup v-if="section[0] === 1" :key="index" :tag-name="section[1]" :markers="section[2]" :atoms="mobiledoc.atoms" :markups="mobiledoc.markups" />-->
+      <ghost-card v-if="section[0] === 10" :key="index" :card="mobiledoc.cards[section[1]]" :data="data" />
     </template>
   </div>
 </div>
@@ -19,7 +19,7 @@ export default {
   mixins: [knowsMarkdown],
   props: ['article', 'data'],
   computed: {
-    doc() {
+    mobiledoc() {
       return JSON.parse(this.article.mobiledoc)
     }
   },
