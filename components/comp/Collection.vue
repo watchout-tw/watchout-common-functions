9e609454143b4e1b962aee64e5a79b9e56ceaca3
chihaoyo <chihaoyo@gmail.com>
2019-01-07 18:20:50 +0800

    Add styling to comp/Collection

--- components/comp/Collection.vue
+++ components/comp/Collection.vue
@@ -1,8 +1,13 @@
 <template>
-<div class="comp-collection">
-  <div>{{ reference }}</div>
-  <div class="items" v-if="collection">
-    <div class="item" v-for="item of collection.items" :key="item.id">{{ item }}</div>
+<div class="comp-collection margin-top-bottom-single">
+  <h4 class="title section-title with-underline small margin-top-bottom-8"><span>{{ collection.title }}</span></h4>
+  <div class="items" v-if="items">
+    <a class="item a-block" v-for="item of items" :key="item.id">
+      <div class="image">{{ item.content.image }}</div>
+      <div class="summary">
+        <h4 class="a-target">{{ item.content.title }}</h4>
+      </div>
+    </a>
   </div>
 </div>
 </template>
@@ -18,7 +23,32 @@ export default {
     },
     collection() {
       return this.data ? this.data[this.reference] : null
+    },
+    items() {
+      return this.collection ? this.collection.items.map(item => {
+        return Object.assign({}, item, {
+          content: this.data[item.reference]
+        })
+      }) : null
     }
   }
 }
 </script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+
+.comp-collection {
+  > .items {
+    > .item {
+      margin: 1rem 0;
+      padding: 0.5rem 0.75rem;
+      background-color: $color-very-very-light-grey;
+      @include shadow;
+      &:first-child {
+        margin-top: 0;
+      }
+    }
+  }
+}
+</style>
