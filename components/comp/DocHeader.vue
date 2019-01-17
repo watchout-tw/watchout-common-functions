25c9d43153bbec9dde608fe45c96d8f97ab0a192
chihaoyo <chihaoyo@gmail.com>
2019-01-17 17:12:35 +0800

    Add titleProcessor; use authorTypes

--- components/comp/DocHeader.vue
+++ components/comp/DocHeader.vue
@@ -1,12 +1,13 @@
 <template>
 <div class="doc-header responsive-typesetting-container margin-top-bottom-single">
   <div class="title variable-font-size margin-top-bottom-single">
-    <h1 class="medium">{{ doc.title }}</h1>
+    <h1 class="medium" v-html="titleProcessor(doc.title)" />
   </div>
-  <div class="authors">
-    <avatar v-for="(author, index) of doc.authors" :persona="cachedAuthorByPersona(author).personaObj" :show="['avatar', 'name']" :classes="['horizontal']" size="small" :link="true" :key="author" />
+  <div v-for="type of authorTypes" :key="type.valuePlural" class="authors" v-if="doc[type.valuePlural]">
+    <div class="section-title with-underline small"><span>{{ type.label }}</span></div>
+    <avatar v-for="(author, index) of doc[type.valuePlural]" :persona="cachedAuthorByPersona(author).personaObj" :show="['avatar', 'name']" :classes="['horizontal']" size="small" :link="true" :key="author" />
   </div>
-  <div class="dates font-size-small">
+  <div class="dates font-size-small margin-top-bottom-8">
     <div><span>發佈時間</span><span class="full-width-punctuation">：</span><span>{{ getDateTimeString(doc.publishedAt) }}</span></div>
     <div v-if="doc.updatedAt"><span>最後更新</span><span class="full-width-punctuation">：</span><span>{{ getDateTimeString(doc.updatedAt) }}</span></div>
   </div>
@@ -14,11 +15,11 @@
 </template>
 
 <script>
-import { knowsFormatting, knowsWatchout } from 'watchout-common-functions/interfaces'
+import { knowsBunko, knowsFormatting, knowsWatchout } from 'watchout-common-functions/interfaces'
 import Avatar from 'watchout-common-functions/components/Avatar'
 
 export default {
-  mixins: [knowsFormatting, knowsWatchout],
+  mixins: [knowsBunko, knowsFormatting, knowsWatchout],
   props: ['doc', 'cachedAuthors'],
   methods: {
     cachedAuthorByPersona(personaID) {
@@ -35,6 +36,7 @@ export default {
 .doc-header {
   > .authors {
     display: flex;
+    align-items: center;
   }
 }
 </style>
--- interfaces/knowsBunko.js
+++ interfaces/knowsBunko.js
@@ -11,6 +11,11 @@ export default {
     getPubDest,
     isPublished(doc) {
       return doc.publishedFrom === 'firestore'
+    },
+    titleProcessor(text) {
+      let punctuations = ['，', '。', '：', '；']
+      let regExp = new RegExp(`([${punctuations.join('')}]+)`, 'g')
+      return text.split(regExp).map(span => regExp.test(span) ? `<span class="full-width-punctuation">${span}</span>` : `<span>${span}</span>`).join('')
     }
   }
 }
