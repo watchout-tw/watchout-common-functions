b9e62d5713abe40a4b83bd9220c8030b496c4b23
chihaoyo <chihaoyo@gmail.com>
2019-01-22 12:29:16 +0800

    Add punctuation

--- interfaces/knowsBunko.js
+++ interfaces/knowsBunko.js
@@ -13,7 +13,7 @@ export default {
       return doc.publishedFrom === 'firestore'
     },
     titleProcessor(text) {
-      let punctuations = ['，', '。', '：', '；']
+      let punctuations = ['，', '。', '、', '：', '；']
       let regExp = new RegExp(`([${punctuations.join('')}]+)`, 'g')
       return text.split(regExp).map(span => regExp.test(span) ? `<span class="full-width-punctuation">${span}</span>` : `<span>${span}</span>`).join('')
     }
