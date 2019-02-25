525a82c0446094f30f2d93512e17952918d39743
chihaoyo <chihaoyo@gmail.com>
2019-02-25 10:40:23 +0800

    Add /comp/Tag

---
+++ components/comp/Tag.vue
@@ -0,0 +1,24 @@
+<template>
+<nuxt-link class="comp-tag a-block" :to="{ name: 'tags-slug', params: { slug: tag.slug } }">
+  <span class="a-target">{{ tag.title }}</span>
+</nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ['tag']
+}
+</script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+
+.comp-tag {
+  font-size: 1rem;
+  line-height: 1;
+  padding: 0.375rem 0.875rem;
+  border-radius: 2rem;
+  margin: 0.25rem;
+  background-color: rgba($color-watchout, 0.5);
+}
+</style>
