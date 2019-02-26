f02ac277963c06351d6db7d7a8d2ee91fde3f199
chihaoyo <chihaoyo@gmail.com>
2019-02-26 11:42:32 +0800

    Move getMusouProjectURL to lib/watchout

--- interfaces/knowsWatchout.js
+++ interfaces/knowsWatchout.js
@@ -1,7 +1,7 @@
 import defaultImage from 'watchout-common-assets/images/default-cover-images/watchout-2-1.jpg'
 import env from 'watchout-common-assets/environment'
 
-import { getBaseURL, getCompDocURL, getCompVideoURL, getWatchoutProjectURL } from 'watchout-common-functions/lib/watchout'
+import { getBaseURL, getCompDocURL, getCompVideoURL, getMusouProjectURL, getWatchoutProjectURL } from 'watchout-common-functions/lib/watchout'
 
 export default {
   methods: {
@@ -45,9 +45,7 @@ export default {
     getParkSettingsURL() {
       return this.getBaseURL('park') + 'settings'
     },
-    getMusouProjectURL(moduleID, projectID) {
-      return this.getBaseURL('musou') + moduleID + '/' + projectID
-    },
+    getMusouProjectURL,
     getWatchoutDocListURL() {
       return this.getBaseURL('watchout') + 'docs'
     },
