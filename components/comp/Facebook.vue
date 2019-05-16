e0dfaa75518868cc175a0002e32c8bb53cbbc32f
chihaoyo <chihaoyo@gmail.com>
2019-05-16 13:39:11 +0800

    Add suport for embedding Facebook post (incomplete); add ratio as a param

--- components/comp/Facebook.vue
+++ components/comp/Facebook.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="comp-facebook tcl-container">
-  <div class="video facebook tcl-panel" v-if="type === TYPES.VIDEO">
-    <div class="video-content-container">
+  <div class="content-wrapper tcl-panel">
+    <div class="content-container" :class="classes">
       <iframe class="content" :src="embedURL" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
     </div>
   </div>
@@ -39,10 +39,21 @@ export default {
       let segments = this.reference.id.split('/')
       return segments[2]
     },
+    classes() {
+      let classes = [this.type]
+      let ratio = '16-9'
+      if(this.reference && this.reference.params && this.reference.params.ratio) {
+        ratio = this.reference.params.ratio
+      }
+      classes.push('ratio-' + ratio)
+      return classes
+    },
     embedURL() {
       let url = null
       if(this.type === TYPES.VIDEO) {
         url = `https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F${this.user}%2Fvideos%2F${this.id}%2F&show_text=false`
+      } else if(this.type === TYPES.POST) {
+        url = `https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F${this.user}%2Fposts%2F${this.id}%2F&show_text=false&width=auto`
       }
       return url
     }
@@ -54,13 +65,31 @@ export default {
 @import '~watchout-common-assets/styles/resources';
 
 .comp-facebook {
-  > .video {
-    > .video-content-container {
-      @include rect(16/9);
-      > .content {
+  > .content-wrapper {
+    > .content-container {
+      background-color: $color-nice-grey;
+      @include shadow-expanded;
+      &.video {
         width: 100%;
-        height: 100%;
-        @include shadow-expanded;
+      }
+      &.post {
+        width: 100%;
+        border-radius: 0.25rem;
+      }
+      &.ratio-2-1 {
+        @include rect(2/1);
+      }
+      &.ratio-16-9 {
+        @include rect(16/9);
+      }
+      &.ratio-1-1 {
+        @include rect(1);
+      }
+      &.ratio-9-16 {
+        @include rect(9/16);
+      }
+      &.ratio-1-2 {
+        @include rect(1/2);
       }
     }
   }
