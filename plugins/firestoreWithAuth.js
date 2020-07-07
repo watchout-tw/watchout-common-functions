953cb5881ae7bd614256887bbcc074a9e5022a92
Charlieeeee <charlieworld2008@gmail.com>
2020-07-07 13:45:11 +0800

    refactor(firestore): add initWithAuth function and plugin

---
+++ plugins/firestoreWithAuth.js
@@ -0,0 +1,15 @@
+import * as firestore from 'watchout-common-functions/lib/firestore'
+
+// initialize firestore
+export default async function({ store, router }) {
+  // init
+  await firestore.sys.initWithAuth(store)
+
+  // cache
+  let [authors, tags] = await Promise.all([
+    firestore.bunko.getAuthors(),
+    firestore.bunko.getTags()
+  ])
+  store.commit('fsCache/setAuthors', authors)
+  store.commit('fsCache/setTags', tags)
+}
