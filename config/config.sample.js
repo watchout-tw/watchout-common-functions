d7ab3dc7e7ec34d7591273fd9a89bbce72fc6089
Wesley <wesley100002001@gmail.com>
2021-09-28 12:01:36 +0800

    Add waa api endpoint

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,11 +1,13 @@
 export default {
   coreBaseURL: 'https://sample.watchout.tw',
   coralreefBaseURL: 'https://sample.watchout.tw',
+  waaBaseURL: 'https://sample.watchout.tw',
   reCaptchaSiteKey: 'google-re-captcha-site-key',
   reCaptchaType: 'invisible',
   ghostBaseURL: 'https://sample.watchout.tw',
   ghostApiURL: 'https://sample.watchout.tw',
   ghostToken: 'ghost-admin-api-key',
+  springboardURL: 'spring-board-url',
   mapboxAccessToken: 'mapbox-access-token',
   imageUploadURL: 'image-upload-url',
   env: 'dev' // dev OR beta OR production
