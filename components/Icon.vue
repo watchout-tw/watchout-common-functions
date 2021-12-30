7c7b1694c96cad5efa6c9cb755f13e92f233e902
Jack Sun <a0972120468@gmail.com>
2021-12-30 14:14:24 +0800

    feat:add edit icon

--- components/Icon.vue
+++ components/Icon.vue
@@ -6,13 +6,13 @@
 
 <script >
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // icon 樣式 請參考以下網址
 // https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free
 
-library.add(faTrashAlt);
+library.add(faTrashAlt, faEdit);
 
 const ICON_NAME_LIST = {
   'trash-alt': 'faTrashAlt',
