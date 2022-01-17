59313cb90359b0101bc09a8ce6391d5a797f53c7
Jack <a0972120468@gmail.com>
2022-01-17 11:15:43 +0800

    feat: add pop up component

---
+++ components/comp/PopUp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="prompt-overlay" :class="[config.classForTheme]" v-if="data.show">
+    <div class="prompt with-dismiss" :class="config.classForContent">
+      <img class="image" v-if="data.image" :src="data.image" alt="image">
+      <div class="primary-secondary-fields font-weight-bold">
+        {{ data.primaryField }}
+        &nbsp;
+        {{ data.secondaryFields }}
+      </div>
+      <div class="title">{{ data.title }}</div>
+      <div class="message">{{ data.description }}</div>
+      <div class="dismiss" @click="dismissPrompt"><span>OK</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+const defaultConfig = {
+  classForTheme: '',
+  classForContent: ''
+}
+
+const defaultData = {
+  show: false,
+  image: '',
+  primaryField: '',
+  secondaryFields: '',
+  title: '',
+  description: ''
+}
+
+export default {
+  props: {
+    config: {
+      type: Object,
+      default: defaultConfig
+    },
+    data: {
+      type: Object,
+      default: defaultData
+    }
+  },
+  methods: {
+    dismissPrompt() {
+      this.data.show = false
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import 'watchout-common-functions/assets/mapbox.scss';
+</style>
