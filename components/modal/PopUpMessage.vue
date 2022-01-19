3ffb6abad4e772daddf1c265b9b0d07e761fc7b0
Jack <a0972120468@gmail.com>
2022-01-19 17:46:22 +0800

    feat: add pop up component for message

--- components/modal/PopUpMessage.vue
+++ components/modal/PopUpMessage.vue
@@ -1,52 +1,34 @@
 <template>
-  <div class="prompt-overlay" :class="[config.classForTheme]" v-if="data.show">
-    <div class="prompt with-dismiss" :class="config.classForContent">
-      <img class="image" v-if="data.image" :src="data.image" alt="image">
+  <div class="prompt-overlay dark">
+    <div class="prompt with-dismiss correct">
+      <img class="image" v-if="data.prompt.image" :src="data.prompt.image" alt="image">
       <div class="primary-secondary-fields font-weight-bold">
-        {{ data.primaryField }}
+        {{ data.prompt.primaryField }}
         &nbsp;
-        {{ data.secondaryFields }}
+        {{ data.prompt.secondaryFields }}
       </div>
-      <div class="title">{{ data.title }}</div>
-      <div class="message">{{ data.description }}</div>
-      <div class="dismiss" @click="dismissPrompt"><span>OK</span></div>
+      <div class="title" v-if="data.prompt.title">{{ data.prompt.title }}</div>
+      <div class="message" v-if="data.prompt.description">{{ data.prompt.description }}</div>
+      <div class="referenceTitle" v-if="data.prompt.isNeedReferences">參考資料</div>
+      <div class="reference" v-for=" ( reference, index ) of data.prompt.references" :key="`reference.text_${index}`">
+        <div class="referenceContent" v-if="reference.text">{{ reference.text }}</div>
+        <a class="referenceLink a-text" v-if="reference.link" :href="reference.link"
+           target="_blank"
+        >
+          {{ reference.link }}
+        </a>
+      </div>
+      <div class="dismiss" @click="removeModal('PopUpMessage')"><span>OK</span></div>
     </div>
   </div>
 </template>
 
 <script>
-
-const defaultConfig = {
-  classForTheme: '',
-  classForContent: ''
-}
-
-const defaultData = {
-  show: false,
-  image: '',
-  primaryField: '',
-  secondaryFields: '',
-  title: '',
-  description: ''
-}
+import { knowsWindowManagement } from 'watchout-common-functions/interfaces'
 
 export default {
-  props: {
-    config: {
-      type: Object,
-      default: defaultConfig
-    },
-    data: {
-      type: Object,
-      default: defaultData
-    }
-  },
-  methods: {
-    dismissPrompt() {
-      this.data.show = false
-    }
-  }
-
+  mixins: [knowsWindowManagement],
+  props: ['data']
 }
 </script>
 
