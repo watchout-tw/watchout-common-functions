2f4fd1b0c229c6daba84ae1634c9f71f49b9b2c2
Jack <a0972120468@gmail.com>
2022-03-01 16:29:35 +0800

    feat: add right arrow and left arrow

--- components/Icon.vue
+++ components/Icon.vue
@@ -4,36 +4,47 @@
   </div>
 </template>
 
-<script >
+<script>
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrashAlt, faEdit, faIcons } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faEdit, faIcons, faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import * as Icon from '../lib/icon'
+
+const ICON_SIZE = {
+  '2xs': '2xs', // 10px
+  'xs': 'xs', // 12px
+  'sm': 'sm', // 14px
+  'lg': 'lg', // 20px
+  'xl': 'xl', // 24px
+  '2xl': '2xl', // 32px
+  '1x': '1x', // 16px
+  '2x': '2x' // 32px
+
+}
 
 // icon 樣式 請參考以下網址
 // https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free
 
-library.add(faTrashAlt, faEdit, faIcons)
+library.add(faTrashAlt, faEdit, faIcons, faAngleRight, faAngleLeft)
 
 export default {
   props: {
     // icon 名稱
     iconName: {
       type: String,
-      default: Icon.ICONS
+      default: 'faIcons'
     },
     // icon 尺寸大小
     iconSize: {
       type: String,
-      default: Icon.SIZE.sm
+      default: ICON_SIZE.sm
     }
   },
   components: {
     FontAwesomeIcon
   }
 }
-</script >
+</script>
 
-<style lang="scss" >
+<style lang="scss">
 @import '~watchout-common-assets/styles/resources';
-</style >
+</style>
