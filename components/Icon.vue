6986468082d2a972e9fccd2cd513c660926c6ce7
Jack <a0972120468@gmail.com>
2022-03-30 13:27:52 +0800

    feat: add bar icon

--- components/Icon.vue
+++ components/Icon.vue
@@ -6,26 +6,26 @@
 
 <script >
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrashAlt, faEdit, faIcons } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faEdit, faIcons, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import * as Icon from '../lib/icon'
+import * as IconTypes from '../lib/icon'
 
 // icon 樣式 請參考以下網址
-// https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free
+// https://fontawesome.com/v5/search?m=free
 
-library.add(faTrashAlt, faEdit, faIcons)
+library.add(faTrashAlt, faEdit, faIcons, faBars)
 
 export default {
   props: {
     // icon 名稱
     iconName: {
       type: String,
-      default: Icon.ICONS
+      default: IconTypes.ICONS
     },
     // icon 尺寸大小
     iconSize: {
       type: String,
-      default: Icon.SIZE.sm
+      default: IconTypes.SIZE.sm
     }
   },
   components: {
