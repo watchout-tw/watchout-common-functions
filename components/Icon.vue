b2975f4302eef4d77394f636b5ea60b224dcd23e
Jack <a0972120468@gmail.com>
2022-03-31 12:04:35 +0800

    feat: mark icon name

--- components/Icon.vue
+++ components/Icon.vue
@@ -8,24 +8,32 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrashAlt, faEdit, faIcons, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import * as IconTypes from '../lib/icon'
 
 // icon 樣式 請參考以下網址
 // https://fontawesome.com/v5/search?m=free
 
 library.add(faTrashAlt, faEdit, faIcons, faBars)
 
+/*********************/
+/*     Icon Name     */
+/*********************/
+
+// faTrashAlt = trash-alt
+// faEdit = edit
+// faIcons = icons
+// faBars = bars
+
 export default {
   props: {
     // icon 名稱
     iconName: {
       type: String,
-      default: IconTypes.ICONS
+      default: 'icons'
     },
     // icon 尺寸大小
     iconSize: {
       type: String,
-      default: IconTypes.SIZE.sm
+      default: '14px'
     }
   },
   components: {
