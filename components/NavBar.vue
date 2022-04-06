6f569f08470f4459d02c1912146c1e77939b64a6
Jack <a0972120468@gmail.com>
2022-04-06 16:41:28 +0800

    feat: add new component menu and auth

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,16 +1,30 @@
 <template>
-<nav class="nav-bar">
-  <nuxt-link class="nav-item logo-with-type" :to="{ name: 'index' }">
-    <img :src="logo" :style="'height: 34px'" />
-  </nuxt-link>
-  <div class="nav-item menu-container">
-    <nav-bar-menu :channel="channel" :menu="menu" :which-side="whichSide" v-if="menu" />
-  </div>
-  <div class="nav-item search-container"></div>
-  <div class="nav-item avatar-container" @click="isCitizen ? addModal('swiss-knife') : addModal({ id: 'auth', joinOrLogin: 'login' })"><client-only>
-    <avatar :show="['avatar']" :persona="activePersona" :parties="parties" />
-  </client-only></div>
-</nav>
+  <nav class="nav-bar" v-if="!isMobile">
+    <nuxt-link class="nav-item logo-with-type" :to="{ name: 'index' }">
+      <img :src="logo" :style="'height: 34px'" />
+    </nuxt-link>
+    <div class="nav-item menu-container">
+      <nav-bar-menu :channel="channel" :menu="menu" :which-side="whichSide" v-if="menu" />
+    </div>
+    <div class="nav-item search-container"></div>
+    <div class="nav-item avatar-container"
+         @click="isCitizen ? addModal('swiss-knife') : addModal({ id: 'auth', joinOrLogin: 'login' })"
+    >
+      <avatar :show="['avatar']" :persona="activePersona" :parties="parties" />
+    </div>
+  </nav>
+
+  <nav class="nav-bar mobile" v-else>
+    <nuxt-link class="nav-item logo-with-type" :to="{ name: 'index' }">
+      <img :src="mobileLogo" :style="'height: 34px'" alt="logo" />
+    </nuxt-link>
+    <div class="nav-item menu-container">
+      <div class="nav-icon" @click="addModal({ id: 'menu-and-auth', menu, isCitizen })">
+        <Icon iconName="bars" iconSize="30px" />
+      </div>
+    </div>
+  </nav>
+
 </template>
 
 <script>
@@ -18,7 +32,9 @@
 import { knowsAuth, knowsWatchout, knowsWindowManagement } from 'watchout-common-functions/interfaces'
 import NavBarMenu from 'watchout-common-functions/components/NavBarMenu'
 import Avatar from 'watchout-common-functions/components/Avatar'
+import Icon from 'watchout-common-functions/components/Icon.vue'
 import logo from 'watchout-common-assets/images/watchout-logo/white-reverse.png'
+import mobileLogo from 'watchout-common-assets/images/watchout-logo/waite-chinese.png'
 
 export default {
   mixins: [knowsAuth, knowsWatchout, knowsWindowManagement],
@@ -26,7 +42,9 @@ export default {
   data() {
     return {
       anon: { id: 'anon', type: 'system' },
-      logo
+      logo,
+      mobileLogo,
+      isMobile: false
     }
   },
   computed: {
@@ -45,15 +63,20 @@ export default {
   created() {
     this.checkAuth()
   },
+  mounted() {
+    this.isMobile = window.innerWidth <= 768
+  },
   components: {
     NavBarMenu,
-    Avatar
+    Avatar,
+    Icon
   }
 }
 </script>
 
 <style lang="scss">
 @import '~watchout-common-assets/styles/resources';
+
 nav.nav-bar {
   position: relative;
   display: flex;
@@ -65,10 +88,34 @@ nav.nav-bar {
   z-index: $z-nav;
   background-color: $color-watchout-black;
 
+  &.mobile {
+    justify-content: space-between;
+  }
+
   > .nav-item {
     flex-grow: 0;
     margin: auto 0 auto 1rem;
+
+    > .nav-icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      margin-right: 1rem;
+
+      > .iconBlock {
+        color: #ffffff;
+        background-color: initial;
+        position: relative;
+        top: initial;
+        right: initial;
+        z-index: initial;
+        border-radius: initial;
+        font-size: 30px;
+      }
+    }
   }
+
   > .logo-with-type {
     flex-shrink: 0;
     display: block;
@@ -78,11 +125,20 @@ nav.nav-bar {
       width: auto;
     }
   }
+
   > .menu-container {
     flex-grow: 1;
     display: flex;
     align-items: center;
+    justify-content: flex-end;
+
+    .item {
+      > a {
+        color: #ffffff;
+      }
+    }
   }
+
   > .avatar-container {
     display: flex;
     flex-basis: 60px;
